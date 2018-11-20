9d281c0389ed71ed467f2fb1daf890e4b05160d2
Sukhbir Singh <Sukhbir.Singh@fdmgroup.com>
2018-11-20 15:37:16 -0500

    first commit

---
+++ src/test/java/com/fdmgroup/test/ArithmeticTest.java
@@ -0,0 +1,45 @@
+package com.fdmgroup.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.fdmgroup.test.model.Arithmetic;
+
+public class ArithmeticTest {
+	static Arithmetic ar;
+	
+	@BeforeClass
+	public  static void setUp(){
+		ar=new Arithmetic(20,10);
+	}
+	
+	@Before
+	public void init(){
+		System.out.println("Initial Stage");
+		
+	}
+	
+	@Test
+	public void addTest(){
+		assertEquals(30, ar.add(20,10));
+	}
+	
+	@Test
+	public void subTest(){
+		assertEquals(10, ar.subTest(20,10));
+	}
+	
+	@After
+	public void finish(){
+		System.out.println("Finish!!!!");
+	}
+	@AfterClass
+	public static void destroy(){
+		System.out.println("Destroy Object!!");
+	}
+}
---
+++ src/test/java/com/fdmgroup/test/model/Arithmetic.java
@@ -0,0 +1,31 @@
+package com.fdmgroup.test.model;
+
+public class Arithmetic {
+
+	private int num1;
+	private int num2;
+	
+	public Arithmetic() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Arithmetic(int num1, int num2) {
+		super();
+		this.num1=num1;
+		this.num2=num2;
+	}
+	public Object add(int i, int j) {
+		// TODO Auto-generated method stub
+		return i+j;
+	}
+	public Object subTest(int i, int j) {
+		// TODO Auto-generated method stub
+		return i-j;
+	}
+
+	
+
+	
+	
+
+}
